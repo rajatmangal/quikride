8559cb264ebf6674cd0c4b4cacd42cf2aa86d34d
Rajat Mangal <rmangal@sfu.ca>
2020-02-19 15:08:13 -0800

    timestamps and styling added to chat page

--- chat/chat.js
+++ chat/chat.js
@@ -1,23 +1,24 @@
 const socketIO = require('socket.io');
+const chatUtil = require('./chat-utils')
 
 function connectChat(server) {
     const io = socketIO(server);
     io.on('connection', (socket) => {
         console.log('New Websocket Connection');
-        socket.emit('message', "Welcome!");
+        socket.emit('message', chatUtil.generateMessage("Welcome!"));
         socket.on('sendMessage', (message, callback) => {
-            io.emit('message', message);
+            io.emit('message', chatUtil.generateMessage(message));
             callback("Delivered");
         });
-        socket.broadcast.emit('message', "A new user has joined");
+        socket.broadcast.emit('message', chatUtil.generateMessage("A new user has joined"));
         socket.on('location', (coords, callback) => {
-            io.emit('locationMessage', `http://google.com/maps?q=${coords.lat},${coords.lon}`);
+            io.emit('locationMessage', chatUtil.generateLink(`http://google.com/maps?q=${coords.lat},${coords.lon}`));
             callback('Location Shared');
         });
 
 
         socket.on('disconnect', () => {
-            io.emit('message', "A user has left")
+            io.emit('message', chatUtil.generateMessage("A user has left"))
         });
     })
 }
