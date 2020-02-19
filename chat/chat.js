224d40ae014bd916b385a3c3e843258dc9d66fb0
Rajat Mangal <rmangal@sfu.ca>
2020-02-19 02:03:59 -0800

    Message and Location display chnages

--- chat/chat.js
+++ chat/chat.js
@@ -5,12 +5,14 @@ function connectChat(server) {
     io.on('connection', (socket) => {
         console.log('New Websocket Connection');
         socket.emit('message', "Welcome!");
-        socket.on('sendMessage', (message) => {
+        socket.on('sendMessage', (message, callback) => {
             io.emit('message', message);
+            callback("Delivered");
         });
         socket.broadcast.emit('message', "A new user has joined");
-        socket.on('location', (coords) => {
-            io.emit('message', `http://google.com/maps?q=${coords.lat},${coords.lon}`)
+        socket.on('location', (coords, callback) => {
+            io.emit('locationMessage', `http://google.com/maps?q=${coords.lat},${coords.lon}`);
+            callback('Location Shared');
         });
 
 
--- public/js/client-chat.js
+++ public/js/client-chat.js
@@ -1,23 +1,57 @@
 const socket = io();
 
+const formData = document.getElementById("formData");
+const messageField =  document.getElementById("message");
+const sendMessage = document.getElementById("sendMessage");
+const sendLoc = document.getElementById("sendLoc");
+const messages = document.getElementById("messages");
+const messageTemplate = document.getElementById("message-template").innerHTML;
+const locationTemplate = document.getElementById("location-template").innerHTML;
 
 socket.on('message', (message) => {
     console.log(message);
+    const html = Mustache.render(messageTemplate, {
+        message: message
+    });
+    messages.insertAdjacentHTML('beforeend',html)
 })
-document.getElementById("formData").addEventListener('submit', (e) => {
-    e.preventDefault();
-    const message = document.getElementById("message").value;
-    socket.emit("sendMessage", message);
+
+socket.on('locationMessage', (url) => {
+    console.log(url)
+    const html = Mustache.render(locationTemplate, {
+        url:url
+    });
+    messages.insertAdjacentHTML('beforeend',html)
 });
 
-document.getElementById("sendLoc").addEventListener('click', (e) => {
+formData.addEventListener('submit', (e) => {
+    e.preventDefault();
+    sendMessage.setAttribute('disabled','disabled');
+    sendLoc.setAttribute('disabled','disabled');
+    const message = messageField.value;
+    socket.emit("sendMessage", message, (message) => {
+        console.log("Message has been delivered", message)
+        sendMessage.removeAttribute('disabled');
+        messageField.value = "";
+        messageField.focus();
+        sendLoc.removeAttribute('disabled');
+    });
+});
+
+sendLoc.addEventListener('click', (e) => {
     if( !navigator.geolocation ) {
         return alert('Geolocation is not supported by your browser.')
     }
     navigator.geolocation.getCurrentPosition( (position) => {
+        sendMessage.setAttribute('disabled','disabled');
+        sendLoc.setAttribute('disabled','disabled');
         socket.emit('location', {
             lat: position.coords.latitude,
             lon: position.coords.longitude
+        }, (message) => {
+            console.log(message);
+            sendMessage.removeAttribute('disabled');
+            sendLoc.removeAttribute('disabled');
         })
     });
 });
