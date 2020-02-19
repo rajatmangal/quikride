4a196e8fe8442cf854f0297a0c6ae07fcfdfc090
Rajat Mangal <rmangal@sfu.ca>
2020-02-19 00:59:30 -0800

    Basic messaging added (via console)

--- chat/chat.js
+++ chat/chat.js
@@ -2,8 +2,21 @@ const socketIO = require('socket.io');
 
 function connectChat(server) {
     const io = socketIO(server);
-    io.on('connection', () => {
-        console.log("New Websocket Connection")
+    io.on('connection', (socket) => {
+        console.log('New Websocket Connection');
+        socket.emit('message', "Welcome!");
+        socket.on('sendMessage', (message) => {
+            io.emit('message', message);
+        });
+        socket.broadcast.emit('message', "A new user has joined");
+        socket.on('location', (coords) => {
+            io.emit('message', `http://google.com/maps?q=${coords.lat},${coords.lon}`)
+        });
+
+
+        socket.on('disconnect', () => {
+            io.emit('message', "A user has left")
+        });
     })
 }
 
--- public/js/client-chat.js
+++ public/js/client-chat.js
@@ -1,2 +1,23 @@
-alert("hi");
-io();
+const socket = io();
+
+
+socket.on('message', (message) => {
+    console.log(message);
+})
+document.getElementById("formData").addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = document.getElementById("message").value;
+    socket.emit("sendMessage", message);
+});
+
+document.getElementById("sendLoc").addEventListener('click', (e) => {
+    if( !navigator.geolocation ) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+    navigator.geolocation.getCurrentPosition( (position) => {
+        socket.emit('location', {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude
+        })
+    });
+});
