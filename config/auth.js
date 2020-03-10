449e86cf93b6bd0e16196e3746661d4bd2a05548
Avneet Toor <avneett@sfu.ca>
2020-03-09 20:48:42 -0700

    Remove absolute URLs for facebook and google sign in

--- config/auth.js
+++ config/auth.js
@@ -2,12 +2,12 @@ module.exports = {
     'facebookAuth' : {
         'clientID': '1505561939618515',
         'clientSecret': '32e78d47e9545277ad172251ae335064',
-        'callbackURL': 'http://localhost:3000/auth/facebook/callback',
+        'callbackURL': '/auth/facebook/callback',
         'profileFields'   : ['displayName', 'name', 'emails']
     },
     'googleAuth' : {
         'clientID': '756343845872-lh8ei1lobtb3sqhr9crgi0m27i9rdku0.apps.googleusercontent.com',
         'clientSecret': 'Wy28CAyHQ5_IsQyTyb9zyVDv',
-        'callbackURL': 'http://localhost:3000/auth/google/callback'
+        'callbackURL': '/auth/google/callback'
     }
 }
